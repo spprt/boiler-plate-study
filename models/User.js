ad81a6322085ca6f40972f2dbb284864c80574e8
supp <kimjiwon1204@gmail.com>
2021-12-16 23:51:17 +0900

    12th token

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -31,6 +33,46 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function( next ) {
+
+    var user = this;
+    // 비밀번호를 암호화 시킴
+    if(user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+    
+            bcrypt.hash(user.password, salt, function(err, hash) { // hash : 암호화된 비밀번호
+                if (err) return next(err)
+                user.password = hash;
+                next()
+            });
+        });
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainPassword 1234567 encrypt $2b$10$2gPcxWM1ARF5xPIDB2/G5.m9fNwyPIp44Y0hiqTmQ7vff7K.zv7tu
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err),
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    //jsonwegtoken 활용 token 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    // user._id + 'secretToken' = token;
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
